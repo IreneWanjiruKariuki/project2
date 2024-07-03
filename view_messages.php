956466bf828fec0bcd2f68b2f84a30f99be81e39
irenekariuki <kariukiirene434@gmail.com>
2024-07-03 20:48:36 +0300

    separating subject line from the message

--- view_messages.php
+++ view_messages.php
@@ -44,7 +44,7 @@
                 <td><?php print $en; ?>.</td>
                 <td><?php print $sel_msg_row["sender_name"]; ?></td>
                 <td><?php print $sel_msg_row["sender_email"]; ?></td>
-                <td><?php print $sel_msg_row["subject_line"]; ?></td>
+                <td><?php print "<strong>". $sel_msg_row["subject_line"] . '</strong> -' . substr($sel_msg_row["text_message"],0,20) . '...'; ?></td>
                 <td><?php print date("d-M-Y H:i", strtotime( $sel_msg_row["datecreated"])); ?></td>
                </tr>
                <?php
